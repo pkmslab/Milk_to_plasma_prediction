ebb57428f3dd05b616443c58d7e4571e38a81d97
akashp21 <apanjabi210@gmail.com>
2024-12-17 19:53:14 -0800

    Multidrug case added to test file

--- tests/testthat/test-MP_prediction_function.R
+++ tests/testthat/test-MP_prediction_function.R
@@ -1,12 +1,24 @@
+# Single drug test
 aspirin <- digest::digest(MP_prediction_function("aspirin")) # digesting the result generated from using aspirin
-custom <- digest::digest(MP_prediction_function("custom")) # digesting the result generated from using the custom feature
 
 test_that("expect_equal test comparing the input of aspirin", {
   expect_equal(digest::digest(MP_prediction_function("aspirin")), aspirin)
 })
 
+
+# Custom drug test
+custom <- digest::digest(MP_prediction_function("custom")) # digesting the result generated from using the custom feature
+
 test_that("expect_equal test comparing the input of custom", {
   expect_equal(digest::digest(MP_prediction_function("custom")), custom)
 })
+
+# Multidrug test
+multi <- digest::digest(MP_prediction_function(c("Metoprolol", "Ketamine", "Codeine"))) # digesting the result generated from using the custom feature
+
+test_that("expect_equal test comparing the input of custom", {
+  expect_equal(digest::digest(MP_prediction_function(c("Metoprolol", "Ketamine", "Codeine"))), multi)
+})
+
 
 
